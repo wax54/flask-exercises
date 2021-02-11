52f55675ee2701f25ad84a444914dbab03a519b2
sam <wax654@gmail.com>
2021-02-10 18:59:30 -0800

    finished up greet project

---
+++ greet/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/welcome")
+def welcome_page():
+    return "welcome"
+
+@app.route("/welcome/home")
+def welcome_home_page():
+    return "welcome home"
+@app.route("/welcome/back")
+def welcome_back_page():
+    return "welcome back"
+
